47df5c15bf306890dc9cf309964fd37c688a3a31
tong <lbuchiyu@gmail.com>
2023-01-06 17:30:49 +0800

    chore: 添加REMDME & .gitignore

--- test.cjs
+++
@@ -1,6 +0,0 @@
-const axios = require('axios')
-
-!(async () => {
-  const data = await axios.get('https://wantwords.net/ChineseRD/?q=%E4%BD%A0%E5%A5%BD&m=ZhZh&f=1')
-  console.log('🚀~ 4  data', data)
-})()
