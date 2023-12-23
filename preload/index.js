ba285a0ce0aaea360dbb3511a5546720e6817f46
tong <lbuchiyu@gmail.com>
2023-12-23 23:19:37 +0800

    feat: 修复请求失败

--- preload/index.js
+++ preload/index.js
@@ -1,13 +1,21 @@
 const axios = require('axios')
 const { features } = require('../plugin.json')
 
+let flag = 'cceb1c9565836a0f3f55f9ac'
+
 const outputMap = {
   汉语: 'ChineseRD',
   英语: 'EnglishRD',
 }
 
-const getBackSetByUrl = async (url) => {
-  const { data = [] } = await axios.get(url)
+const tryUrl = async (url) => {
+  const { data = [] } = await axios.get(url, {
+    method: "get",
+    headers: {
+      flag,
+      'X-Requested-With': 'XMLHttpRequest',
+    }
+  })
   if (Array.isArray(data)) {
     window.backSetList = data.slice(0, 100).map(item => ({
       title: item.w,
@@ -19,6 +27,23 @@ const getBackSetByUrl = async (url) => {
       title: data.message,
       description: data.details,
     }]
+  }
+}
+
+const getBackSetByUrl = async (url) => {
+  try {
+    await tryUrl(url)
+  } catch (e) {
+    const res = await axios.get('https://wantwords.net/static/js/home.js')
+    flag = /const flag = '(.*?)';/.exec(res?.data)?.[1]
+    if (flag) {
+      await tryUrl(url)
+    } else {
+      window.backSetList = [{
+        title: '获取失败',
+        description: '获取失败，请联系插件开发者',
+      }]
+    }
   }
 }
 
