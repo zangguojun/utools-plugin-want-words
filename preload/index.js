2d0c897db336d6fc2f5e961c3aceb106f770787a
tong <lbuchiyu@gmail.com>
2024-03-22 21:50:53 +0800

    feat: 优化flag获取逻辑 & 新增enter复制选择文本

--- preload/index.js
+++ preload/index.js
@@ -1,75 +1,44 @@
 const axios = require('axios')
 const { features } = require('../plugin.json')
 
+// 后续可能失效
 let flag = 'cceb1c9565836a0f3f55f9ac'
+
+const service = axios.create({
+  baseURL: 'https://wantwords.net'
+});
+
+service.interceptors.request.use(
+  config => {
+    config.headers.flag = flag;
+    return config;
+  },
+  error => {
+    console.error(error);
+    Promise.reject(error);
+  }
+);
+
+// 响应拦截器
+service.interceptors.response.use(
+  undefined,
+  async (error) => {
+    const { response } = error
+    if (response.status === 403) {
+      const { data: jsText } = await service.get('/static/js/home.js')
+      flag = /const flag = '(.*?)';/.exec(jsText)?.[1]
+    }
+    return Promise.reject(error);
+  }
+);
 
 const outputMap = {
   汉语: 'ChineseRD',
   英语: 'EnglishRD',
 }
 
-const tryUrl = async (url) => {
-  const { data = [] } = await axios.get(url, {
-    method: "get",
-    headers: {
-      flag,
-      'X-Requested-With': 'XMLHttpRequest',
-    }
-  })
-  if (Array.isArray(data)) {
-    window.backSetList = data.slice(0, 100).map(item => ({
-      title: item.w,
-      description: item.d,
-      url: `https://openhownet.thunlp.org/search?keyword=${item.w}`
-    }))
-    try {
-      const w = data.slice(0, 100).map(it => it?.w).join('|')
-      const { data: descData = [] } = await axios.post('https://wantwords.net/GetChDefis/', {
-        w,
-        m: 0
-      }, {
-        headers: {
-          flag,
-          'X-Requested-With': 'XMLHttpRequest',
-          'Content-Type': 'application/json'
-        }
-      })
-
-      window.backSetList = data.map((item, index) => ({
-        title: item.w,
-        description: descData?.[index]?.Z?.map(([, v, desc], i) => `${i + 1}.${v ? `[${v}]` : ''}${desc}`).join(''),
-        url: `https://openhownet.thunlp.org/search?keyword=${item.w}`
-      }))
-
-    } catch (e) {}
-
-  } else {
-    window.backSetList = [{
-      title: data.message,
-      description: data.details,
-    }]
-  }
-}
-
-const getBackSetByUrl = async (url) => {
-  try {
-    await tryUrl(url)
-  } catch (e) {
-    const res = await axios.get('https://wantwords.net/static/js/home.js')
-    flag = /const flag = '(.*?)';/.exec(res?.data)?.[1]
-    if (flag) {
-      await tryUrl(url)
-    } else {
-      window.backSetList = [{
-        title: '获取失败',
-        description: '获取失败，请联系插件开发者',
-      }]
-    }
-  }
-}
-
-const praise = async (selectData, originData = window.backSetList[0]) => {
-  await axios.get(`https://wantwords.net/feedback/?c=${selectData.title}|2|||${originData.title}&m=w`)
+const praise = (selectData, originData = window.backSetList[0]) => {
+  return service.get(`/feedback/?c=${selectData.title}|2|||${originData.title}&m=w`)
 }
 
 window.exports = features.reduce((pre, cur) => {
@@ -79,14 +48,46 @@ window.exports = features.reduce((pre, cur) => {
     mode: "list",
     args: {
       search: async (action, searchWord, callbackSetList) => {
-        await getBackSetByUrl(`https://wantwords.net/${outputMap[output]}/?q=${searchWord}&m=${code}&f=1`)
+        const { data = [] } =  await service.get(`${outputMap[output]}/?q=${searchWord}&m=${code}&f=1`)
+        if (Array.isArray(data)) {
+          window.backSetList = data.slice(0, 100).map(item => ({
+            title: item.w,
+            description: item.d,
+            url: `https://openhownet.thunlp.org/search?keyword=${item.w}`
+          }))
+          try {
+            const w = data.slice(0, 100).map(it => it?.w).join('|')
+            const { data: descData = [] } = await service.post('/GetChDefis/', {
+              w,
+              m: 0
+            }, {
+              headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Content-Type': 'application/json'
+              }
+            })
+
+            window.backSetList = data.map((item, index) => ({
+              title: item.w,
+              description: descData?.[index]?.Z?.map(([, v, desc], i) => `${i + 1}.${v ? `[${v}]` : ''}${desc}`).join(''),
+              url: `https://openhownet.thunlp.org/search?keyword=${item.w}`
+            }))
+
+          } catch (e) {}
+
+        } else {
+          window.backSetList = [{
+            title: data.message,
+            description: data.details,
+          }]
+        }
         callbackSetList(window.backSetList)
       },
       select: async (action, itemData) => {
-        window.utools.copyText(itemData.title)
-        window.utools.hideMainWindow()
+        window.utools?.copyText(itemData.title)
+        window.utools?.hideMainWindow()
         await praise(itemData)
-        window.utools.outPlugin()
+        window.utools?.outPlugin()
       },
       placeholder: `请输入${input}`
     }
